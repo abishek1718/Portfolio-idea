e501fddef8e136bf194f0b7daf696cecaf1bf56b
Abishek kanageswaran <kanage01@email.franklin.edu>
2020-11-28 14:39:31 -0500

    Movie Detail

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { MovieState } from "../movieState";
-//Animation
+//Animations
 import { motion } from "framer-motion";
 import { pageAnimation } from "../animation";
 
@@ -12,10 +12,11 @@ const MovieDetail = () => {
   const [movies, setMovies] = useState(MovieState);
   const [movie, setMovie] = useState(null);
 
-  //use effect
+  //UseEffect
   useEffect(() => {
     const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
     setMovie(currentMovie[0]);
+    console.log(setMovies);
   }, [movies, url]);
 
   return (
@@ -49,12 +50,9 @@ const MovieDetail = () => {
   );
 };
 
-//styled components
-
 const Details = styled(motion.div)`
   color: white;
 `;
-
 const HeadLine = styled.div`
   min-height: 90vh;
   padding-top: 20vh;
@@ -71,19 +69,17 @@ const HeadLine = styled.div`
     object-fit: cover;
   }
 `;
-
 const Awards = styled.div`
   min-height: 80vh;
   display: flex;
-  margin: 5em 10rem;
+  margin: 5rem 10rem;
   align-items: center;
   justify-content: space-around;
   @media (max-width: 1500px) {
     display: block;
-    margin: 2rem 1rem;
+    margin: 2rem 2rem;
   }
 `;
-
 const AwardStyle = styled.div`
   padding: 5rem;
   h3 {
@@ -95,11 +91,10 @@ const AwardStyle = styled.div`
     height: 0.5rem;
     margin: 1rem 0rem;
   }
-  P {
+  p {
     padding: 2rem 0rem;
   }
 `;
-
 const ImageDisplay = styled.div`
   min-height: 50vh;
   img {
@@ -109,8 +104,7 @@ const ImageDisplay = styled.div`
   }
 `;
 
-//award component
-
+//Award Component
 const Award = ({ title, description }) => {
   return (
     <AwardStyle>
